4988b4d282474ac1173b6e533f575f9b30d0de73
gajenthiran <gajenthirank3@gmail.com>
2024-12-20 11:02:11 +0530

    routing and form

--- src/app/user-management/add-user/add-user.component.ts
+++ src/app/user-management/add-user/add-user.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.scss'
+  styleUrl: './add-user.component.scss',
 })
-export class AddUserComponent {
+export class AddUserComponent implements OnInit {
+  addUserForm!: FormGroup;
+  userDetails: any[]= [];
 
+  ngOnInit(): void {
+    this.addUserForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phoneNo: new FormControl('', Validators.required),
+    });
+  }
+
+  storeUserDate() {
+    if (this.addUserForm.valid) {
+      const { userName, email, phoneNo } = this.addUserForm.value;
+      const userDate = { userName, email, phoneNo}
+      this.userDetails.push(userDate);
+      this.addUserForm.reset();
+      console.log(this.userDetails);
+    }
+  }
 }
--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import { AddUserComponent } from './add-user/add-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AddUserComponent } from './add-user/add-user.component';
   ],
   imports: [
     CommonModule,
-    UserManagementRoutingModule
+    UserManagementRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UserManagementModule { }
